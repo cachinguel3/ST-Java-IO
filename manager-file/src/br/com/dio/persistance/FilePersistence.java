33b391e3576287dc8ff5624878fe157433cea842
Eduardo_A. <edu.crecenzio.1003@gmail.com>
2026-02-22 21:20:42 -0300

    interface

---
+++ manager-file/src/br/com/dio/persistance/FilePersistence.java
@@ -0,0 +1,14 @@
+package br.com.dio.persistance;
+
+public interface FilePersistence {
+
+    String write(final String data);
+
+    boolean remove(final String sentence);
+
+    String replace(final String oldContent, final String newContent);
+
+    String findAll();
+
+    String findBy(final String sentence);
+}
