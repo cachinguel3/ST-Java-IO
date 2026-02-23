47698ba8ffe8f909715ecc9d3767691fe3f2b72b
Eduardo_A. <edu.crecenzio.1003@gmail.com>
2026-02-22 21:20:52 -0300

    IOFilePersistence

---
+++ manager-file/src/br/com/dio/persistance/IOFilePersistence.java
@@ -0,0 +1,123 @@
+package br.com.dio.persistance;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class IOFilePersistence implements FilePersistence{
+
+    private final String currentDir = System.getProperty("user.dir");
+
+    private String storedDir = "/managedFiles/IO/";
+
+    private final String fileName;
+
+    public IOFilePersistence(String fileName) throws IOException {
+        this.fileName = fileName;
+        var file = new File(currentDir + storedDir);
+        if(!file.exists() && !file.mkdirs()) throw new IOException("Erro ao criar arquivo");
+
+        clearFile();
+
+    }
+
+    @Override
+    public String write(final String data) {
+        try(
+                var fileWriter = new FileWriter(currentDir + storedDir + fileName, true);
+                var bufferedWrite = new BufferedWriter(fileWriter);
+                var printWriter = new PrintWriter(bufferedWrite);
+                ) {
+            printWriter.println(data);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return data;
+    }
+
+    @Override
+    public boolean remove(final String sentence) {
+        var contentList = toListString();
+
+        if(contentList.stream().noneMatch(c -> c.contains(sentence))) return false;
+
+        clearFile();
+        contentList.stream()
+                .filter(c -> !c.contains(sentence))
+                .forEach(this::write);
+        return true;
+    }
+
+    @Override
+    public String replace(final String oldContent, final String newContent) {
+        var contentList = toListString();
+
+        if(contentList.stream().noneMatch(c -> c.contains(oldContent))) return "";
+
+        clearFile();
+        contentList.stream()
+                .map(c -> c.contains(oldContent) ? newContent : c)
+                .forEach(this::write);
+        return newContent;
+    }
+
+    @Override
+    public String findAll() {
+        var content = new StringBuilder();
+        try(var reader = new BufferedReader(new FileReader(currentDir + storedDir + fileName))){
+            String line;
+            do {
+                line = reader.readLine();
+                if (line != null) content.append(line)
+                        .append(System.lineSeparator());
+
+            } while (line != null);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return content.toString();
+    }
+
+    @Override
+    public String findBy(final String sentence) {
+        String found = "";
+        try (var reader = new BufferedReader(new FileReader(currentDir + storedDir + fileName))){
+            String line;
+            do {
+                line = reader.readLine();
+                if (line != null && line.contains(sentence)) found = line;
+            } while (line != null);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return found;
+    }
+
+    private ArrayList<String> toListString() {
+        var content = findAll();
+        return new ArrayList<>(Stream.of(content.split(System.lineSeparator())).toList());
+    }
+
+    private void clearFile(){
+        try(OutputStream outputStream = new FileOutputStream(currentDir + storedDir + fileName)) {
+            //System.out.printf("Inicialzando recurso %s \n", currentDir + storedDir + fileName);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+
+        }
+    }
+
+    private void createFile(){
+
+    }
+}
